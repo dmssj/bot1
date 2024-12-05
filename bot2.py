26c68f6473ee0b8bc5ebef6e2f23f5b5792a6883
dmssj <letrey19@gmail.com>
2024-12-06 01:54:53 +0700

    2

--- bot2.py
+++ bot2.py
@@ -4,6 +4,6 @@ bot = telebot.TeleBot('')
 
 @bot.message_handler(content_types=['voice'])
 def voice_handler(message):
-    bot.send_message(message.chat.id, "спасибо за гс")
+    bot.send_message(message.chat.id, "Спасибо за гс")
 
 bot.polling()
