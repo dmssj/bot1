57701f1dfc5a5d82ec8ed5ec752c86ff55579667
dmssj <letrey19@gmail.com>
2024-12-06 01:55:42 +0700

    4

--- bot4.py
+++ bot4.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot('')
 def start(message):
     # Создание клавиатуры
     keyboard = types.ReplyKeyboardMarkup()
-    keyboard.add(types.KeyboardButton("напиши 'Закрыть' в сообщении бота"))
+    keyboard.add(types.KeyboardButton("напиши 'Закрыть' в сообщении"))
     bot.send_message(message.chat.id, "напиши 'Закрыть', чтобы убрать клавиатуру.", reply_markup=keyboard)
 
 @bot.message_handler(func=lambda message: message.text == "Закрыть")
