06b4eebf635d09af6a01f288b13115967586d71b
dmssj <letrey19@gmail.com>
2024-12-06 02:04:40 +0700

    1

--- bot1.py
+++ bot1.py
@@ -1,11 +1,11 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('***')
+bot = telebot.TeleBot('')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    keyboard = types.ReplyKeyboardMarkup()
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(types.KeyboardButton("Сайты по тестированию"))
     keyboard.add(types.KeyboardButton("Сайты по программированию"))
     keyboard.add(types.KeyboardButton("Сайты по дизайну"))
@@ -14,10 +14,27 @@ def start(message):
 @bot.message_handler(func=lambda message: True)
 def send_links(message):
     if message.text == "Сайты по тестированию":
-        bot.send_message(message.chat.id, "1. https://testautomationu.applitools.com\n2. https://www.softwaretestinghelp.com")
+        bot.send_message(
+            message.chat.id,
+            "Сайты по тестированию:\n"
+            "1. [Test Automation University](https://testautomationu.applitools.com) - курсы по автоматизации тестирования.\n"
+            "2. [Software Testing Help](https://www.softwaretestinghelp.com) - статьи и советы для тестировщиков."
+        )
     elif message.text == "Сайты по программированию":
-        bot.send_message(message.chat.id, "1. https://stackoverflow.com\n2. https://github.com")
+        bot.send_message(
+            message.chat.id,
+            "Сайты по программированию:\n"
+            "1. [Stack Overflow](https://stackoverflow.com) - форум для программистов, где можно найти ответы на вопросы.\n"
+            "2. [GitHub](https://github.com) - платформа для хранения и совместной работы над проектами."
+        )
     elif message.text == "Сайты по дизайну":
-        bot.send_message(message.chat.id, "1. https://dribbble.com\n2. https://www.behance.net")
+        bot.send_message(
+            message.chat.id,
+            "Сайты по дизайну:\n"
+            "1. [Dribbble](https://dribbble.com) - галерея работ дизайнеров.\n"
+            "2. [Behance](https://www.behance.net) - портфолио и проекты креативных специалистов."
+        )
+    else:
+        bot.send_message(message.chat.id, "Выберите категорию с клавиатуры.")
 
 bot.polling()
